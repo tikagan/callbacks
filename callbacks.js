8893d29ea68f37ee220cccdb77a886618f60703f
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-09-07 16:01:21 +0000

    anonFunctions inital code, no edits

--- callbacks.js
+++ callbacks.js
@@ -1,12 +1,21 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      const index = i
-      found(index)   // execute callback
+  console.log(arr)
+  arr.forEach(function(element, index)) {
+    if (element === "Waldo") {
+      console.log(element)
+      found(index)
     }
   }
 }
+
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       const index = i
+//       found(index)   // execute callback
+//     }
+//   }
+// }
 
 function actionWhenFound(index) {
   console.log("Found him at index " + index + "!")
